839e160efe874b5ac34a278d42f1c30c2fd59b41
boss160250@gmail.com <boss160250@gmail.com>
2024-12-24 01:41:33 +0700

    Almost done

---
+++ cmd/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
+
+func input() *tview.InputField {
+	inputfield := tview.NewInputField().
+		SetLabel("Add: ")
+	return inputfield
+}
+
+func textView() *tview.TextView {
+	text := tview.NewTextView().
+		SetTextColor(tcell.ColorHotPink).
+		SetTextAlign(tview.AlignCenter)
+	return text
+}
+
+func main() {
+	app := tview.NewApplication()
+	inputfield := input()
+	text := textView()
+	text.SetBorder(true)
+	text.SetTitle("TODO LIST")
+	text.SetBackgroundColor(tcell.ColorBlack)
+	text.SetBorderColor(tcell.ColorRoyalBlue)
+	text.SetDrawFunc(func(screen tcell.Screen, x int, y int, width int, height int) (int, int, int, int) {
+		// Draw a horizontal line across the middle of the box
+		centerY := y + height/5
+		for cx := x + 1; cx < x+width-1; cx++ {
+			screen.SetContent(cx, centerY, tview.BoxDrawingsLightHorizontal, nil, tcell.StyleDefault.Foreground(tcell.ColorWhite))
+		}
+		tview.Print(screen, " Center Line ", x+1, centerY-1, width-2, tview.AlignLeft, tcell.ColorYellow)
+
+		return x + 1, centerY + 2, width - 2, height - (centerY + 1 - y)
+	})
+
+	inputfield.SetDoneFunc(func(key tcell.Key) {
+ if key == tcell.KeyEnter {
+            currentText := text.GetText(false) // Get the current text without regions
+            newText := currentText + "\n" + inputfield.GetText()
+            text.SetText(newText) // Update the TextView with the updated text
+            inputfield.SetText("") // Clear the input field
+        }
+	})
+
+	flex := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(text, 0, 1, false).
+		AddItem(inputfield, 1, 1, true) // Set inputfield to be in focus initially
+
+	if err := app.SetRoot(flex, true).SetFocus(inputfield).Run(); err != nil {
+		panic(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,5 +1,15 @@
-module github.com/ibossbabu/Mtodo
+module todo
 
 go 1.23.4
 
-require github.com/Shopify/go-lua v0.0.0-20240527182111-9ab1540f3f5f // indirect
+require (
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.7.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/tview v0.0.0-20241103174730-c76f7879f592 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/term v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
