122fbb329bd623a753545e410a2520007d9cd57a
boss160250@gmail.com <boss160250@gmail.com>
2024-12-24 23:58:01 +0700

    ADDED MODE AND INTERACTION

--- cmd/main.go
+++ cmd/main.go
@@ -1,48 +1,80 @@
 package main
 
 import (
+	"todo/customize"
+	"todo/table"
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
-  "todo/cmd/customize"
 )
 
 func input() *tview.InputField {
 	inputfield := tview.NewInputField().
-		SetLabel("Add: ")
+		SetLabel("Add: "). 
+    SetFieldBackgroundColor(tcell.ColorRoyalBlue)
 	return inputfield
 }
 
-
 func main() {
-  text := customize.TextView()
+	text := customize.TextView()
 	app := tview.NewApplication()
 	inputfield := input()
-	text.SetDrawFunc(func(screen tcell.Screen, x int, y int, width int, height int) (int, int, int, int) {
-		// Draw a horizontal line across the middle of the box
-		centerY := y + height/5
-		for cx := x + 1; cx < x+width-1; cx++ {
-			screen.SetContent(cx, centerY, tview.BoxDrawingsLightHorizontal, nil, tcell.StyleDefault.Foreground(tcell.ColorWhite))
-		}
-		tview.Print(screen, " Center Line ", x+1, centerY-1, width-2, tview.AlignLeft, tcell.ColorYellow)
-
-		return x + 1, centerY + 2, width - 2, height - (centerY + 1 - y)
-	})
-
+	tbl := table.Table()
+	row := 0 
+  updateMode := func(mode string) {
+        text.SetText(mode)
+    }
 	inputfield.SetDoneFunc(func(key tcell.Key) {
- if key == tcell.KeyEnter {
-            currentText := text.GetText(false) // Get the current text without regions
-            newText := currentText + "\n" + "\n"+ inputfield.GetText()
-            text.SetText(newText) // Update the TextView with the updated text
-            inputfield.SetText("") // Clear the input field
-        }
+		if key == tcell.KeyEnter {
+			newText := inputfield.GetText()
+			if newText != "" {
+				cell := tview.NewTableCell(newText).
+					SetExpansion(1).          // This makes the cell expand to fill space
+					SetAlign(tview.AlignLeft) 
+
+				tbl.SetCell(row, 0, cell)
+				row++
+				inputfield.SetText("") // Clear the input field
+				// Move selection to new row
+				tbl.Select(row-1, 0)
+			}
+		}
 	})
 
 	flex := tview.NewFlex().
 		SetDirection(tview.FlexRow).
+		AddItem(tbl, 0, 6, false).
 		AddItem(text, 0, 1, false).
-		AddItem(inputfield, 1, 1, true) // Set inputfield to be in focus initially
+		AddItem(inputfield, 1, 1, true)
 
-	if err := app.SetRoot(flex, true).SetFocus(inputfield).Run(); err != nil {
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		switch event.Key() {
+		case tcell.KeyRune:
+			if event.Rune() == 'i' && app.GetFocus() != inputfield {
+				app.SetFocus(inputfield)
+        updateMode("INSERT")
+				return nil
+			}
+      if event.Rune() == 'q' && app.GetFocus() != inputfield {
+				app.Stop()
+				return nil
+      }
+      if event.Rune() == 'v' && app.GetFocus() != inputfield{
+        app.SetFocus(tbl)
+        updateMode("VISUAL")
+        return nil
+      }
+		case tcell.KeyEsc:
+			app.SetFocus(nil)
+      updateMode("NORMAL")
+			// Ensure there's at least one row to select
+			if row > 0 {
+				tbl.Select(tbl.GetSelection())
+			}
+		}
+		return event
+	})
+
+	if err := app.SetRoot(flex, true).SetFocus(nil).Run(); err != nil {
 		panic(err)
 	}
 }
